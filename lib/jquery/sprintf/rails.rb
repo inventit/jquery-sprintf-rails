c5fdb0823e4aa60e7baada62bda27fb54a0d7914
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 13:54:05 +0900

    initial commit

---
+++ lib/jquery/sprintf/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/sprintf/rails/version"
+
+module Jquery
+  module Sprintf
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery/sprintf/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Sprintf
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
